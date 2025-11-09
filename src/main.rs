b237959c8f6359f0b897ad07962a9daab1a686bc
Kim Hong Rae <midra03@naver.com>
2025-11-09 12:14:46 +0900

    fix: template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::io::{stdin, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut it = input.split_ascii_whitespace();
 }
