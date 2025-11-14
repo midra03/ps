1793bf46cc5294cfa9a8059950021e3e603555c5
Kim Hong Rae <midra03@naver.com>
2025-11-14 19:18:53 +0900

    fix: Update rust template

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::io::{stdin, Read};
+use std::io::{stdin, Write, Read};
 
 fn main() {
     let mut input = String::new();
     stdin().read_to_string(&mut input).unwrap();
     let mut it = input.split_ascii_whitespace();
+    let stdout = std::io::stdout();
+    let mut buf = std::io::BufWriter::new(stdout.lock());
 }
